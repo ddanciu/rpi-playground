2d1e58418bcc7b9038208a548a8861379e8ba0a9
Dan Danciu <ddanciuster@gmail.com>
2017-10-10 01:11:06 +0300

    new code for l293d motor shield (not tested)

---
+++ py3-playground/motor_shield2.py
@@ -0,0 +1,90 @@
+import RPi.GPIO as IO
+import time
+from enum import Enum
+
+from shift_register import ShiftRegister 
+
+class Motors(Enum):
+    MOTOR0A = 1
+    MOTOR0B = 2
+    MOTOR2A = 3
+    MOTOR2B = 4
+
+class Direction(Enum):
+    FORWARD = 1
+    BACKWARD = 2
+    RELEASE = 3
+
+
+class DCMotor:
+
+    _pwm = None
+    _direction = Direction.RELEASE
+    _enc = {Direction.RELEASE : 0}
+
+    def __init__(self, wire, forward=0, backword=0):
+        self._pwm = IO.PWM(wire, self._PWM_FREQ)
+        self._pwm.start(0)
+
+        self._enc[Direction.FORWARD] = forward
+        self._enc[Direction.BACKWARD] = backword
+
+
+    def setSpeed(self, speed):
+        self._pwm.ChangeDutyCycle(speed)
+
+    def setDirection(self, direction):
+        self._direction = direction
+
+    def getDirectionEncoding(self):
+        return self._enc[self._direction]
+
+
+class MotorShield:
+    _t = 0.01
+    _PWM_FREQ = 50
+    _hc595 = ShiftRegister()
+    _motors = {}
+
+    def __init__(self, t=0.01):
+        IO.setmode (IO.BCM)
+        self._t = t
+
+    def wireDirection(self, data, clock, latch, reset):
+        self._hc595.wire(data, clock, latch, reset)
+
+    def wireDCMotors(self, pwm2A=None, pwm2B=None, pwm0A=None, pwm0B=None):    
+        if pwm2A:
+            self.motors[self.MOTOR2A] = DCMotor(pwm2A, forward=4, reverse=8)
+
+        if pwm2B:
+            self.motors[self.MOTOR2B] = DCMotor(pwm2B, forward=2, reverse=16)
+
+        if pwm0A:
+            self.motors[self.MOTOR0A] = DCMotor(pwm0A, forward=1, reverse=64)
+    
+        if pwm0B:
+            self.motors[self.MOTOR0B] = DCMotor(pwm0B, forward=32, reverse=128)
+
+
+    def wireServos(self, pwm1A, pwm1B):
+        print("Not implemented")
+
+
+    def setSpeed(self, speed, *motors):
+        for which in motors:
+            self._motors[which].setSpeed(speed)
+
+    def run(self, direction=Direction.RELEASE, *motors):
+
+        for which in self._motors:
+
+            if which in motors 
+                self._motors[which].setDirection(direction)
+
+            no += self.motors[which].getDirectionEncoding()
+
+        self._hc595.out(no)
+
+
+
